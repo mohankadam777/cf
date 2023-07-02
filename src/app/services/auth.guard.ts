da6606778fb2b2e4ecff6055ec2eff0f176ad519
Mohan <mohankadam61@gmail.com>
2023-07-02 21:41:39 +0530

    second

--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -13,21 +13,6 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-//     let permit = this.userServices.checkToken().subscribe({
-//       next:(res:any)=>{
-//         console.log(res);
-        
-// return false;
-//       },
-//       error:(err:any)=>{
-//         console.log(err);
-        
-// this.router.navigate(['/unsecure'])
-// return false;
-//       }
-//     });
-    // console.log("auth guard: >> ",permit);
-    // console.log(per);
     
     return false;
   }
